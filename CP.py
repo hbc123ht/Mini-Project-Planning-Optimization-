e2f1e6ec360b2988bb76f75cac6d978a4fd3fd8e
hbc123ht <hbc123ht@gmail.com>
2023-02-03 10:46:41 +0700

    nothing to say

--- CP.py
+++ CP.py
@@ -1,86 +1,102 @@
-"""Simple solve."""
 from ortools.sat.python import cp_model
 
-def prepare_input():
-    f = open("SolomonPotvinBengio/rc_207.4.txt", "r")
-    N = int(f.readline())
-    t = list()
-    R = list()
-    D = list()
-    for i in range(N):
-        Ti = f.readline().split(' ')
-        for j in range(N):
-            t.append(int(float(Ti[j])))
-
-    for i in range(N):
-        TW = list(filter(None, f.readline().split(' ')))
-        R.append(int(float(TW[0])))
-        D.append(int(float(TW[1])))
-
-    return N, t, R, D 
+def prepare_input(path):
+    with open(path, 'r') as file: 
+        n = int(file.readline()) + 1
+        early = []
+        late = []
+        delay = []
+        cost = []
+        time = []
+        for i in range(n):
+            e, l, d = file.readline().strip().split(' ')
+            early.append(int(e))
+            late.append(int(l))
+            delay.append(int(d))
+            # delay.append(0)
+        for i in range(n):
+            for a in file.readline().strip().split(' '):
+                cost.append(int(a))
+        for i in range(n):
+            for a in file.readline().strip().split(' '):
+                time.append(int(a))
+    data = {}
+    data['n'] = n
+    data['early'] = early
+    data['late'] = late
+    data['delay'] = delay
+    data['cost'] = cost
+    data['time'] = time
+    return data
     
 
 def main():
     """Minimal CP-SAT example to showcase calling the solver."""
     # Creates the model.
     model = cp_model.CpModel()
-
     # init 
-    N, t, R, D = prepare_input()
-    max_boundary = max(D)
+    data = prepare_input('Data/25points_3days.txt')
 
     # Creates the variables.
     P = list()
-    for i in range(N):
-        P.append(model.NewIntVar(0, N - 1, f'P_{i}'))
+    for i in range(data['n']):
+        P.append(model.NewIntVar(0, data['n'] - 1, f'P_{i}'))
 
     T = list()
-    for i in range(N):
-        T.append(model.NewIntVar(0, max_boundary, f'P_{i}'))
+    for i in range(data['n']):
+        T.append(model.NewIntVar(0, max(data['late']), f'T_{i}'))
 
     
     # Init Tp
     T_P = list()
-    for i in range(N):
+    for i in range(data['n']):
         # T_P[i] = T[P[i]]
-        T_P.append(model.NewIntVar(0, max_boundary, f'T_P_{i}'))
+        T_P.append(model.NewIntVar(0, max(data['late']), f'T_P_{i}'))
         model.AddElement(P[i], T, T_P[i])
-
+        
     # init P_i_i+1
     P_i_i1 = list()
-    for i in range(N - 1):
-        P_i_i1.append(model.NewIntVar(0, max_boundary, f'P_i_i1{i}'))
-        model.Add(P_i_i1[i] == P[i] * N + P[i + 1])
+    for i in range(data['n'] - 1):
+        P_i_i1.append(model.NewIntVar(0, data['n'] ** 2, f'P_i_i1{i}'))
+        model.Add(P_i_i1[i] == P[i] * data['n'] + P[i + 1])
 
     # init t_p_p
     t_P_P = list()
-    for i in range(N-1):
-        # t_P_P[i] = t[P[i] * N + P[j]]
-        t_P_P.append(model.NewIntVar(0, max_boundary, f't_P_P{i}'))
-        model.AddElement(P_i_i1[i], t, t_P_P[i])
+    cost_P_P = list()
+    delay_P = list()
+    for i in range(data['n']-1):
+        # t_P_P[i] = t[P[i] * N + P[i + 1]]
+        t_P_P.append(model.NewIntVar(0, max(data['time']), f't_P_P{i}'))
+        model.AddElement(P_i_i1[i], data['time'], t_P_P[i])
+        # cost_P_P[i] = data['cost'][P[i] * N + P[i + 1]]
+        cost_P_P.append(model.NewIntVar(0, max(data['cost']), f't_P_P{i}'))
+        model.AddElement(P_i_i1[i], data['cost'], cost_P_P[i])
+        # delay_P[i] = delay[P[i]]
+        delay_P.append(model.NewIntVar(0, max(data['delay']), f'delay_P_{i}'))
+        model.AddElement(P[i], data['delay'], delay_P[i])
 
     # init R_P, D_P
     R_P = list()
     D_P = list()
-    for i in range(N):
-        R_P.append(model.NewIntVar(0, max_boundary, f'T_P_{i}'))
-        D_P.append(model.NewIntVar(0, max_boundary, f'D_P_{i}'))
-        model.AddElement(P[i], R, R_P[i])
-        model.AddElement(P[i], D, D_P[i])
+    for i in range(data['n']):
+        R_P.append(model.NewIntVar(0, max(data['late']), f'T_P_{i}'))
+        D_P.append(model.NewIntVar(0, max(data['late']), f'D_P_{i}'))
+        model.AddElement(P[i], data['early'], R_P[i])
+        model.AddElement(P[i], data['late'], D_P[i])
 
     # Creates the constraints.
     
     model.AddAllDifferent(P)
 
     model.Add(P[0] == 0)
-    for i in range(N - 1):
-        model.Add(T_P[i] + t_P_P[i] <= T_P[i + 1])
+    for i in range(data['n'] - 1):
+        model.Add(T_P[i] + t_P_P[i] + delay_P[i] <= T_P[i + 1])
 
-    for i in range(N):
+    for i in range(data['n']):
         model.Add(T_P[i] >= R_P[i])
         model.Add(T_P[i] <= D_P[i])
 
-    model.Minimize(T_P[N - 1] - T_P[0])
+    model.Minimize(sum(cost_P_P[i] for i in range(data['n'] - 1)))
 
     # Creates a solver and solves the model.
     solver = cp_model.CpSolver()
@@ -91,10 +107,10 @@ def main():
         
         # for i in range(1, N):
         #     print(f'P_{i} = {solver.Value(P[i])}, T_P_{i} = {solver.Value(T_P[i])}, t_P_P{i} = {solver.Value(t_P_P[i - 1])}')
-        for i in range(N):
-            print(solver.Value(P[i]), solver.Value(T_P[i]), R[solver.Value(P[i])], D[solver.Value(P[i])])
+        # for i in range(data['n']):
+        #     print(solver.Value(P[i]), solver.Value(T_P[i]), data['early'][solver.Value(P[i])], data['late'][solver.Value(P[i])])
         with open('sol.txt', 'w') as f:
-            for i in range(1, N):
+            for i in range(1, data['n']):
                 f.write(f'{solver.Value(P[i])} ')
     else:
         print('No solution found.')
--- MIP.py
+++ MIP.py
@@ -1,31 +1,37 @@
 from ortools.linear_solver import pywraplp
 
-def prepare_input():
-    f = open("SolomonPotvinBengio/rc_207.4.txt", "r")
-    N = int(f.readline())
-    t = list()
-    R = list()
-    D = list()
-    for i in range(N):
-        Ti = f.readline().split(' ')
-        for j in range(N):
-            t.append(int(float(Ti[j])))
-        t.append(0)
-
-    for i in range(N):
-        TW = list(filter(None, f.readline().split(' ')))
-        R.append(int(float(TW[0])))
-        D.append(int(float(TW[1])))
-        # R.append(0)
-        # D.append(1000)
-    R.append(0)
-    D.append(10000)
-    return N, t, R, D 
-
+def prepare_input(path):
+    with open(path, 'r') as file: 
+        n = int(file.readline()) + 1
+        early = []
+        late = []
+        delay = []
+        cost = []
+        time = []
+        for i in range(n):
+            e, l, d = file.readline().strip().split(' ')
+            early.append(int(e))
+            late.append(int(l))
+            delay.append(int(d))
+            # delay.append(0)
+        for i in range(n):
+            for a in file.readline().strip().split(' '):
+                cost.append(int(a))
+        for i in range(n):
+            for a in file.readline().strip().split(' '):
+                time.append(int(a))
+    data = {}
+    data['n'] = n
+    data['early'] = early
+    data['late'] = late
+    data['delay'] = delay
+    data['cost'] = cost
+    data['time'] = time
+    return data
+    
 def main():
     # init 
-    N, t, R, D = prepare_input()
-    max_boundary = max(D)
+    data = prepare_input('Data/25points_3days.txt')
 
     # Create the mip solver with the SCIP backend.
     solver = pywraplp.Solver.CreateSolver('SCIP')
@@ -35,48 +41,43 @@ def main():
     # Creates the variables.
     # x_i_j
     x = list()
-    for i in range(N):
-        for j in range(N + 1):
+    for i in range(data['n']):
+        for j in range(data['n']):
             x.append(solver.IntVar(0, 1, 'x_{}_{}'.format(i, j)))
 
     # t_i
     T = list()
-    for i in range(N + 1):
-        T.append(solver.IntVar(0, max_boundary, 'T_{}'.format(i)))
+    for i in range(data['n']):
+        T.append(solver.IntVar(0, max(data['late']), 'T_{}'.format(i)))
 
 
     # constraints
-    for i in range(1, N):
-        solver.Add(T[i] - T[0] - t[0 * (N + 1) + i] * x[0 * (N + 1) + i] >= 0)
+    for i in range(1, data['n']):
+        solver.Add(T[i] - T[0] - data['time'][0 * data['n'] + i] * x[0 * data['n'] + i] >= 0)
 
-    for i in range(0, N):
-        solver.Add(T[i] >= R[i])
+    for i in range(data['n']):
+        solver.Add(x[i * data['n'] + i] == 0)
 
-    for i in range(N):
-        solver.Add(x[i * (N + 1) + i] == 0)
+    for i in range(data['n']):
+        solver.Add(solver.Sum(x[i * data['n'] + j] for j in range(data['n'])) == 1)
 
-    for i in range(N):
-        solver.Add(solver.Sum(x[i * (N + 1) + j] for j in range(N + 1)) == 1)
+    for i in range(1, data['n']):
+        solver.Add(solver.Sum(x[j * data['n'] + i] for j in range(data['n'])) == 1)
 
-    for i in range(1, N + 1):
-        solver.Add(solver.Sum(x[j * (N + 1) + i] for j in range(N)) == 1)
-
-    solver.Add(x[N] == 0)
-
-    for i in range(1, N):
-        for j in range(1, N + 1):
+    for i in range(data['n']):
+        for j in range(1, data['n']):
             if (i == j):
                 continue
-            solver.Add(T[i] - T[j] + (D[i] - R[j] + t[i * (N + 1) + j]) * x[i * (N + 1) + j] <= D[i] - R[j])
+            solver.Add(T[i] - T[j] + (data['late'][i] - data['early'][j] + data['delay'][i] + data['time'][i * data['n'] + j]) * x[i * data['n'] + j] <= data['late'][i] - data['early'][j])
 
-    for i in range(0, N):
-        solver.Add(T[i] <= D[i])
+    for i in range(0, data['n']):
+        solver.Add(T[i] >= data['early'][i])
+        solver.Add(T[i] <= data['late'][i])
 
 
     print('Number of constraints =', solver.NumConstraints())
 
-    solver.Minimize(T[N] - T[0])
-
+    solver.Minimize(solver.Sum(data['cost'][i] * x[i] if i % data['n'] else 0 for i in range(data['n'] ** 2)))
 
     status = solver.Solve()
 
@@ -84,11 +85,11 @@ def main():
         print('Solution:')
         print('Objective value =', solver.Objective().Value())
 
-        for i in range(N + 1):
-            print(T[i].solution_value(), R[i], D[i])
+        # for i in range(data['n']):
+        #     print(T[i].solution_value(), data['early'][i], data['late'][i])
 
-        for i in range(N):
-            print([x[i * (N + 1) + j].solution_value() for j in range(N + 1)])
+        # for i in range(data['n']):
+        #     print([x[i * data['n'] + j].solution_value() for j in range(data['n'])])
     else:
         print('The problem does not have an optimal solution.')
 
